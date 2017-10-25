2da1d0bab8375b03589a95167034b849a31a69ab
itbdw <itbudaoweng@gmail.com>
2017-10-25 16:20:00 +0800

    log headers

--- App/Libraries/Curl.php
+++ App/Libraries/Curl.php
@@ -19,6 +19,7 @@ class Curl
     public static $http_code = null;
     public static $error_code = null;
     public static $error_msg = null;
+    public static $headers = null;
 
     /**
      * @param $url
@@ -157,6 +158,7 @@ class Curl
         self::$error_code = curl_errno($ch);
         self::$error_msg = curl_error($ch);
         self::$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        self::$headers = curl_getinfo($ch);
 
         if (self::$error_code) {
             self::error($url, self::$error_code . ' ' . self::$error_msg);
@@ -211,97 +213,12 @@ class Curl
         return $output;
     }
 
-    /**
-     * @param $url
-     * @param array $curl_params
-     * @return array
-     */
-    public static function getHeadersBeautyLowerCase($url, $curl_params = [])
-    {
-        $headers = Curl::getHeadersBeauty($url, $curl_params);
-        foreach ($headers as $k => $v) {
-            unset($headers[$k]);
-            $headers[strtolower($k)] = $v;
-        }
-
-        return $headers;
-    }
-
-    /**
-     * @param $url
-     * @param array $curl_params
-     * @return array
-     *
-     * {
-     * Http-Header: "HTTP/1.1 200 OK",
-     * Date: "Tue, 05 Jul 2016 04",
-     * Server: "nginx",
-     * Content-Type: "image/gif",
-     * Content-Length: "14437898",
-     * Accept-Ranges: "bytes",
-     * Access-Control-Allow-Origin: "*",
-     * Access-Control-Expose-Headers: "X-Log, X-Reqid",
-     * Access-Control-Max-Age: "2592000",
-     * Cache-Control: "public, max-age=7200",
-     * Content-Disposition: "inline; filename="ff59817bf2a5bbb678d71a57486e90da.gif"",
-     * Content-Transfer-Encoding: "binary",
-     * ETag: ""loZkShTothYHyLM8VmVVy_Bxari5"",
-     * Last-Modified: "Mon, 04 Jul 2016 10",
-     * X-Log: "mc.g;IO",
-     * X-Reqid: "exMAAM5VC8bSEV4U",
-     * X-Qiniu-Zone: "0",
-     * X-Via: "1.1 zhouwangtong155",
-     * Connection: "keep-alive"
-     * }
-     */
-    public static function getHeadersBeauty($url, $curl_params = [])
-    {
-        $return = Curl::getHeaders($url, $curl_params);
-        $return = explode("\r\n", $return);
-
-        $scheme = array_shift($return);
-        array_unshift($return, 'Http-Header:' . $scheme);
-        $return = array_filter($return);
-
-        $headers = [];
-        foreach ($return as $r) {
-            list($k, $v) = explode(":", $r);
-            $headers[trim($k)] = trim($v);
-        }
-
-        if (!isset($headers['Http-Header']) || !$headers['Http-Header']) {
-            $headers['Http-Header'] = 'CUSTOEM-ERROR TIME OUT';
-        }
-
-        return $headers;
-    }
 
     /**
      * @param $url
      * @param $curl_params
      * @return mixed
      *
-     * HTTP/1.1 200 OK
-     * Date: Tue, 05 Jul 2016 04:04:48 GMT
-     * Server: nginx
-     * Content-Type: image/gif
-     * Content-Length: 14437898
-     * Accept-Ranges: bytes
-     * Access-Control-Allow-Origin: *
-     * Access-Control-Expose-Headers: X-Log, X-Reqid
-     * Access-Control-Max-Age: 2592000
-     * Cache-Control: public, max-age=7200
-     * Content-Disposition: inline; filename="ff59817bf2a5bbb678d71a57486e90da.gif"
-     * Content-Transfer-Encoding: binary
-     * ETag: "loZkShTothYHyLM8VmVVy_Bxari5"
-     * Last-Modified: Mon, 04 Jul 2016 10:34:16 GMT
-     * X-Log: mc.g;IO:1
-     * X-Reqid: Z1EAALPQ5y5bEV4U
-     * X-Qiniu-Zone: 0
-     * X-Via: 1.1 chengwtong85:8105 (Cdn Cache Server V2.0), 1.1 lianwangtong61:4 (Cdn Cache Server V2.0)
-     * Connection: keep-alive
-     * {此处有一个 \r\n}
-     * {此处有一个 \r\n}
      */
     public static function getHeaders($url, $curl_params = [])
     {
@@ -322,12 +239,12 @@ class Curl
 
         curl_setopt_array($ch, $result_params);
 
-        $output = curl_exec($ch);
+        curl_exec($ch);
 
         self::logResponse($url, $ch);
 
         curl_close($ch);
-        return $output;
+        return self::$headers;
     }
 
 }
